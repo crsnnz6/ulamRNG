2e9ef8a7c153e2378294c1348fd85c09795f5d94
crsnnz6 <126161848+crsnnz6@users.noreply.github.com>
2024-02-28 18:48:58 +0800

    modified:   src/App.vue 	new file:   src/Wheel.vue 	deleted:    src/components/Wheel.vue

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,9 @@
 <script setup>
-  import Wheel from './components/Wheel.vue';
   import UlamCard from './components/UlamCard.vue';
 </script>
 <template>
   This is the main page
   <br/>
-  <Wheel/>
   <br/>
   <UlamCard/>
 </template>
