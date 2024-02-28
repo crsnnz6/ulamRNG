560aaf78a064870bfedffe2605aa6424ebffa6ea
crsnnz6 <126161848+crsnnz6@users.noreply.github.com>
2024-02-28 23:23:54 +0800

    modified:   package-lock.json 	modified:   package.json 	modified:   src/App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,38 @@
 <script setup>
-import {ref} from "vue"
-import ulamData from "./data/ulam.json"
-import UlamCard from './components/UlamCard.vue';
+import { ref } from "vue";
+import ulamData from "./data/ulam.json";
+import { v4 as uuidv4 } from 'uuid';
 
-const ulams = ref(ulamData);
+if (!localStorage.getItem('ulamData')) {
+  localStorage.setItem('ulamData', JSON.stringify(ulamData));
+}
+
+const ulams = ref(JSON.parse(localStorage.getItem('ulamData')));
+const newUlam = ref({})
+
+const addUlam = (newUlamObj) => {
+  newUlamObj.id = uuidv4()
+
+  ulams.value.push(newUlamObj)
+  const updatedUlamData = JSON.stringify(ulams.value)
+  localStorage.setItem('ulamData', updatedUlamData)
+  newUlam.value = {}
+}
+
+const deleteUlam = (ulamId) => {
+  ulams.value = ulams.value.filter(ulam => ulam.id !== ulamId)
+  const updatedUlamData = JSON.stringify(ulams.value)
+  localStorage.setItem('ulamData', updatedUlamData)
+}
 </script>
 <template>
   This is the main page
+  <div>
+    <input v-model="newUlam.ulamName" type="text" placeholder="Ulam Name"/>
+    <input v-model="newUlam.imageURL" type="text" placeholder="Ulam Image Link"/>
+    <input v-model="newUlam.ytLink" type="text" placeholder="Ulam YT video Link"/>
+    <button @click="addUlam(newUlam)">Add Ulam</button>
+  </div>
   <div v-for="ulam in ulams" class="card">
     <div class="card-header">
       <h2>{{ ulam.ulamName }}</h2>
@@ -15,7 +41,9 @@ const ulams = ref(ulamData);
       <img :src="ulam.imageURL" alt="Ulam Image" height="100" width="100">
       <p>Click <a :href="ulam.ytLink" target="_blank">here</a> for the tutorial.</p>
     </div>
+    <button @click="deleteUlam(ulam.id)">Delete</button>
   </div>
+
 </template>
 
 <style>
