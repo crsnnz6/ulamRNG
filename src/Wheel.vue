bd573e14cf3768178c31bfda69c4bf97adb7c7e0
kyleconsebido <123555531+kyleconsebido@users.noreply.github.com>
2024-02-28 20:05:23 +0800

    Ulam Modal

--- src/Wheel.vue
+++ src/Wheel.vue
@@ -1,3 +1,14 @@
+<script setup>
+import { ref } from "vue";
+import UlamModal from "./components/UlamModal.vue";
+
+const openModal = ref(false);
+const selectedUlam = ref(null);
+
+</script>
+
 <template>
     This is the wheel page.
+
+    <UlamModal v-if="openModal" :onClose="() => openModal = false" :ulam="selectedUlam" />
 </template>
