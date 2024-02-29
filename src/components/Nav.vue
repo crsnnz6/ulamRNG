514ffa75735f9372dd9a5a5fcc57718e92af16d1
Noah <yannicknoah.tuano@outlook.com>
2024-02-29 09:29:22 +0800

    new file:   src/Home.vue 	new file:   src/components/Nav.vue 	new file:   src/components/UlamHomeCard.vue

---
+++ src/components/Nav.vue
@@ -0,0 +1,46 @@
+<template>
+    <nav>
+      <ul>
+        <li><router-link to="/">Home</router-link></li>
+        <li><router-link to="/generate">Generate</router-link></li>
+      </ul>
+    </nav>
+  </template>
+  
+  <script setup>
+  import { defineProps } from 'vue';
+  import { RouterLink } from 'vue-router';
+  
+  const props = defineProps({
+    // Props definition goes here (if any)
+  });
+  </script>
+  
+  <style>
+  /* Optional styling for the navbar */
+  nav {
+    background-color: #333;
+    padding: 10px 0;
+  }
+  
+  ul {
+    list-style-type: none;
+    padding: 0;
+    margin: 0;
+  }
+  
+  li {
+    display: inline;
+    margin-right: 10px;
+  }
+  
+  a {
+    color: white;
+    text-decoration: none;
+  }
+  
+  a:hover {
+    text-decoration: underline;
+  }
+  </style>
+  
