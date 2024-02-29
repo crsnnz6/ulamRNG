deee2f8c89cfa1602b74a61bd4dbc64d7f770e21
crsnnz6 <creseedeljean.nunez.cics@ust.edu.ph>
2024-02-29 08:26:28 +0800

    modified:   src/App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,48 @@
 <script setup>
 import { ref } from "vue";
 import ulamData from "./data/ulam.json";
-import { v4 as uuidv4 } from 'uuid';
+import { v4 as uuidv4 } from "uuid";
 
-if (!localStorage.getItem('ulamData')) {
-  localStorage.setItem('ulamData', JSON.stringify(ulamData));
+if (!localStorage.getItem("ulamData")) {
+  localStorage.setItem("ulamData", JSON.stringify(ulamData));
 }
 
-const ulams = ref(JSON.parse(localStorage.getItem('ulamData')));
-const newUlam = ref({})
+const ulams = ref(JSON.parse(localStorage.getItem("ulamData")));
+const newUlamIngredients = ref("");
+const newUlam = ref({});
 
 const addUlam = (newUlamObj) => {
-  newUlamObj.id = uuidv4()
+  newUlamObj.id = uuidv4();
+  newUlamObj.ingredients = newUlamIngredients.value.split(", ");
 
-  ulams.value.push(newUlamObj)
-  const updatedUlamData = JSON.stringify(ulams.value)
-  localStorage.setItem('ulamData', updatedUlamData)
-  newUlam.value = {}
-}
+  ulams.value.push(newUlamObj);
+  const updatedUlamData = JSON.stringify(ulams.value);
+  localStorage.setItem("ulamData", updatedUlamData);
+  newUlam.value = {};
+  newUlamIngredients.value = "";
+};
 
 const deleteUlam = (ulamId) => {
-  ulams.value = ulams.value.filter(ulam => ulam.id !== ulamId)
-  const updatedUlamData = JSON.stringify(ulams.value)
-  localStorage.setItem('ulamData', updatedUlamData)
-}
+  ulams.value = ulams.value.filter((ulam) => ulam.id !== ulamId);
+  const updatedUlamData = JSON.stringify(ulams.value);
+  localStorage.setItem("ulamData", updatedUlamData);
+};
 </script>
 <template>
   This is the main page
   <div>
-    <input v-model="newUlam.ulamName" type="text" placeholder="Ulam Name"/>
-    <input v-model="newUlam.imageURL" type="text" placeholder="Ulam Image Link"/>
-    <input v-model="newUlam.ytLink" type="text" placeholder="Ulam YT video Link"/>
+    <input v-model="newUlam.ulamName" type="text" placeholder="Ulam Name" />
+    <input
+      v-model="newUlam.imageURL"
+      type="text"
+      placeholder="Ulam Image Link"
+    />
+    <input
+      v-model="newUlam.ytLink"
+      type="text"
+      placeholder="Ulam YT video Link"
+    />
+    <input v-model="newUlamIngredients" type="text" placeholder="Ingredients" />
     <button @click="addUlam(newUlam)">Add Ulam</button>
   </div>
   <div v-for="ulam in ulams" class="card">
@@ -38,12 +50,13 @@ const deleteUlam = (ulamId) => {
       <h2>{{ ulam.ulamName }}</h2>
     </div>
     <div class="card-body">
-      <img :src="ulam.imageURL" alt="Ulam Image" height="100" width="100">
-      <p>Click <a :href="ulam.ytLink" target="_blank">here</a> for the tutorial.</p>
+      <img :src="ulam.imageURL" alt="Ulam Image" height="100" width="100" />
+      <p>
+        Click <a :href="ulam.ytLink" target="_blank">here</a> for the tutorial.
+      </p>
     </div>
     <button @click="deleteUlam(ulam.id)">Delete</button>
   </div>
-
 </template>
 
 <style>
